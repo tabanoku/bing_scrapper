c623345203c766877061eb4a6e74cc80d9efd45e
tabanoku <tabanoku@gmail.com>
2022-03-17 22:58:13 +0100

    Exports to JSON file

--- main.py
+++ main.py
@@ -1,58 +1,34 @@
-#import os, json
-import threading, requests
+import threading, requests, json
 from bs4 import BeautifulSoup
 
-def main():
-    array = ("filetype%3Apdf+test", "enthec")
-#    pathfile = _file_creator()
 
+def main():
+    array = ('filetype%3Apdf+test', 'enthec')
+    pathfile = 'results.json'
     for search_item in array:
-        t = threading.Thread(target=_scrapper(search_item))
-#        t = threading.Thread(target=_scrapper(search_item, pathfile))
+        t = threading.Thread(target=_scrapper(search_item, pathfile))
         t.start()
 
-# def _file_creator():
-#     #abspath =  '.' + os.path.abspath(os.getcwd())
-#     file_name = 'results.json'
+def _json_writer(result_list: list, pathfile: str):
+    '''
+    Escribe los resultados en el archivo JSON
+    '''
 
-#     if os.path.isfile(file_name):
-        
-#         expand = 1
-        
-#         while True:
-#             expand += 1
-#             new_file_name = file_name.split('.json')[0] + str(expand) + '.json'
-           
-#             if os.path.isfile(new_file_name):
-#                 continue
-#             else:
-#                 file_name = new_file_name
-#                 break
-        
-#         f = open(file_name, 'w+')
-#         f.write('[]')
-#         f.close()
+    for item in result_list:
+        document = {
+            'title':item[0],
+            'url':item[1]
+        }
 
-#     return(file_name)
-
-# def _json_writer(result_list: list, pathfile: str):
-#     for item in result_list:
-#         document = {
-#             'title':item[0],
-#             'url':item[1]
-#         }
-
-#         with open(pathfile) as reader:
-#             data = json.load(reader)
-#             data.append(document)
-#         with open(pathfile, 'w') as writer:
-#             json.dump(data, writer, indent=4)
-
-#def _scrapper(term: str, pathfile: str):
+        with open(pathfile) as reader:
+            data = json.load(reader)
+            data.append(document)
+        with open(pathfile, 'w') as writer:
+             json.dump(data, writer, indent=4)
 
 
+def _scrapper(term: str, pathfile: str):
 
-def _scrapper(term: str):
     '''
     Bing scrapper, obtiene los resultados y los muestra por pantalla (Titulo y URL)
     '''
@@ -74,8 +50,7 @@ def _scrapper(term: str):
         result_list.append([result.get_text(), result.get_attribute_list("href")])
 
     print(result_list)
- #   _json_writer(result_list, pathfile)
-
+    _json_writer(result_list, pathfile)
 
 
 if __name__ == '__main__':
